f9882bb8e9fcd2a86d1b12fb7bec5de3e2b0028a
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-02-03 00:43:45 +0530

    medblock ui init

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
+  import "medblocks-ui"
+  import "medblocks-ui/dist/styles.js"
+  import "medblocks-ui/dist/shoelace" 
 
 </script>
 <div>
-  
+  <mb-input label='text input'></mb-input>
+
 </div>
